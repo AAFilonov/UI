ffa67f2601bcbe0b76117aa07972b360b268e7f1
filon13p <filon13p@gmail.com>
2021-02-11 11:53:04 +0300

    все вроде сделал

--- Lab1/Lab1/Form1.Designer.cs
+++ Lab1/Lab1/Form1.Designer.cs
@@ -29,31 +29,32 @@ namespace Lab1
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusTime = new System.Windows.Forms.ToolStripStatusLabel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Distance = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Size = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.number = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonReset = new System.Windows.Forms.Button();
             this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(338, 218);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // statusStrip1
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1,
             this.toolStripStatusTime});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 498);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(1224, 22);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -68,28 +69,137 @@ namespace Lab1
             this.toolStripStatusTime.Name = "toolStripStatusTime";
             this.toolStripStatusTime.Size = new System.Drawing.Size(0, 17);
             // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(151, 14);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Дистанция:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Размер:";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(151, 38);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 4;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.number,
+            this.Distance,
+            this.Size,
+            this.Time});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(19, 64);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(252, 396);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Distance
+            // 
+            this.Distance.Text = "Дистанция";
+            // 
+            // Size
+            // 
+            this.Size.Text = "Размер";
+            // 
+            // Time
+            // 
+            this.Time.Text = "Время";
+            // 
+            // number
+            // 
+            this.number.Text = "№ Опыта";
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(196, 466);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 7;
+            this.buttonReset.Text = "Сброс";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1224, 520);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusTime;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader number;
+        private System.Windows.Forms.ColumnHeader Distance;
+        private System.Windows.Forms.ColumnHeader Size;
+        private System.Windows.Forms.ColumnHeader Time;
+        private System.Windows.Forms.Button buttonReset;
     }
 }
 
--- Lab1/Lab1/Form1.cs
+++ Lab1/Lab1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,24 +13,72 @@ namespace Lab1
 {
     public partial class Form1 : Form
     {
+        Stopwatch stopwatch;
+        Random rnd;
+        RoundButton newButton;
         public Form1()
         {
             InitializeComponent();
+            this.MouseClick += mouseClickHandler;
+            stopwatch = new Stopwatch();
+            rnd = new Random();
+            count = 0;
+
+             newButton = new RoundButton();
+            this.Controls.Add(newButton);
+            //newButton.Text = "Кнопка";
+           
+            newButton.Visible = false;
+            newButton.FlatStyle = FlatStyle.Flat;
+            newButton.BackColor = Color.Gray;
+            newButton.FlatAppearance.BorderSize = 0;
+            newButton.Click += newBtnClickHandler;
+        }
+       
+        private void mouseClickHandler(object sender, MouseEventArgs e)
+        {
+            var Distance = (int)numericUpDown1.Value;
+            var Size = (int)numericUpDown2.Value;
+
+
+            var StartPosition = e.Location;
+
+            var rad = rnd.NextDouble() * (Math.PI * 2);
+            Point point = new Point(
+                (int)(Distance * Math.Sin(rad)),
+                (int)(Distance * Math.Cos(rad))
+                );
+
+            newButton.Size = new Size(Size, Size);
+            newButton.Location = new Point(StartPosition.X + point.X, StartPosition.Y + point.Y);
+            newButton.Visible = true;
+
+            stopwatch.Reset();
+            stopwatch.Start();
+
+        }
+        int count;
+        private void newBtnClickHandler(object sender, EventArgs e)
+        {
+            stopwatch.Stop();
+            count++;
+
+            var Distance = (int)numericUpDown1.Value;
+            var Size = (int)numericUpDown2.Value;
+            var Time = stopwatch.ElapsedMilliseconds;
+          
+            toolStripStatusTime.Text = Time + " мс";
+            newButton.Visible = false;
+
+            ListViewItem item = new ListViewItem(new string[] { count.ToString(), Distance.ToString(), Size.ToString(), Time.ToString() });
+            listView1.Items.Add(item);
+
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void buttonReset_Click(object sender, EventArgs e)
         {
-            var StartPosition = (Button)sender;
-            var newButton = new RoundButton();
-            this.Controls.Add(newButton);
-            newButton.Text = "Кнопка";
-            newButton.Location = new Point(StartPosition.Location.X+ 200, StartPosition.Location.Y+0);
-            newButton.Size = new Size(50, 50);
-            newButton.Visible = true;
-            newButton.FlatStyle = FlatStyle.Flat
-            newButton.FlatAppearance.BorderColor = Color.White;
-            newButton.FlatAppearance.BorderSize = 1;
-
+            count = 0;
+            listView1.Items.Clear();
         }
     }
 }
